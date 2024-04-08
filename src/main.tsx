f211d6c07cecdfb397cf03ddacf72126e41201ae
orlandodiazc <orlandodiazconde@gmail.com>
2024-04-07 21:07:12 -0500

    add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import "@fontsource-variable/hanken-grotesk";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <h1 className="text-red-500">test</h1>
   </React.StrictMode>,
 );
