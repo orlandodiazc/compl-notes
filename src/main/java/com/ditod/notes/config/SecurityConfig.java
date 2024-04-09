58c1daa3f55ccd8b77a937609319bad68f78e41e
orlandodiazc <orlandodiazconde@gmail.com>
2024-04-09 16:51:27 -0500

    Update allowed origins

--- src/main/java/com/ditod/notes/config/SecurityConfig.java
+++ src/main/java/com/ditod/notes/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http.cors(c -> {
             c.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:5173"));
+                config.setAllowedOrigins(List.of("http://localhost:5173", "https://compl-notes.onrender.com/"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
                 config.setAllowedHeaders(List.of("*"));
                 return config;
