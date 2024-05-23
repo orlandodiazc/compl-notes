25a8142c7cac5361fa4fd4ca99d77296aaa50114
orlandodiazc <orlandodiazconde@gmail.com>
2024-05-23 10:39:43 -0500

    Refactor image headers to helper class

--- src/main/java/com/ditod/notes/domain/note_image/NoteImageController.java
+++ src/main/java/com/ditod/notes/domain/note_image/NoteImageController.java
@@ -1,40 +1,34 @@
 package com.ditod.notes.domain.note_image;
 
 import com.ditod.notes.domain.exception.EntityNotFoundException;
-import org.springframework.http.*;
+import com.ditod.notes.utils.ImageUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/note-images")
 public class NoteImageController {
     private final NoteImageRepository noteImageRepository;
+    private final ImageUtils imageUtils;
 
-    public NoteImageController(NoteImageRepository noteImageRepository) {
+    public NoteImageController(NoteImageRepository noteImageRepository,
+            ImageUtils imageUtils) {
         this.noteImageRepository = noteImageRepository;
+        this.imageUtils = imageUtils;
     }
 
     @GetMapping("/{imageId}")
-    ResponseEntity<?> oneNoteImage(@PathVariable UUID imageId) {
+    ResponseEntity<byte[]> getNoteImage(@PathVariable UUID imageId) {
         NoteImage userImage = noteImageRepository.findById(imageId)
                 .orElseThrow(() -> new EntityNotFoundException("note image", imageId));
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.setContentType(MediaType.parseMediaType(userImage.getContentType()));
-        responseHeaders.setContentLength(userImage.getBlob().length);
-        responseHeaders.setContentDisposition(ContentDisposition.builder("inline")
-                .filename(imageId.toString())
-                .build());
-        responseHeaders.setCacheControl(CacheControl.maxAge(Duration.ofDays(365))
-                .cachePublic()
-                .immutable());
 
         return ResponseEntity.ok()
-                .headers(responseHeaders)
+                .headers(imageUtils.getImageResponseHeaders(userImage.getId(), userImage.getContentType(), userImage.getBlob().length))
                 .body(userImage.getBlob());
     }
 
