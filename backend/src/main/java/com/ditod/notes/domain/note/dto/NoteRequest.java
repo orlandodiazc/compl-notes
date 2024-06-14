17b74b1781257b95b87b09e8d4a6a7d132d87ade
orlandodiazc <orlandodiazconde@gmail.com>
2024-06-14 12:35:37 -0500

    Update NoteRequest validation

--- backend/src/main/java/com/ditod/notes/domain/note/dto/NoteRequest.java
+++ backend/src/main/java/com/ditod/notes/domain/note/dto/NoteRequest.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class NoteRequest {
     @NotNull(message = "Title is required")
-    @Size(min = 1, max = 100, message = "Title length must be between 1 and 100 characters")
+    @Size(min = 1, max = 50, message = "Title length must be between 1 and 100 characters")
     private String title;
     @NotNull(message = "Content is required")
-    @Size(min = 1, max = 10000, message = "Content length must be between 1 and 10000 characters")
+    @Size(min = 1, max = 1000, message = "Content length must be between 1 and 10000 characters")
     private String content;
     @Size(max = 5, message = "The maximum number of images is 5")
     private List<NoteImageRequest> images;
